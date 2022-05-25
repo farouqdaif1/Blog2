074a015c0f22f7e33ee04b723bfcac4e9b6e2830
farouqdaif1 <daiffarouq@gamil.com>
2022-05-26 00:58:29 +0200

    Fix test

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -3,7 +3,12 @@ require 'rails_helper'
 RSpec.describe 'Posts', type: :request do
   describe 'GET #index' do
     before(:each) do
-      get user_posts_path
+      @user = User.create(name: 'farouq', photo: 'image', bio: 'Developer from Egypt', email: 'daiffarouq@email.com',
+                          password: 'password', confirmed_at: Time.now)
+      @post = Post.create(user: @user, title: 'title', text: 'text')
+      @comment = Comment.create(text: 'My first comment', user: @user, post_id: @post.id)
+      @like = Like.create(user: @user, post_id: @post.id)
+      get user_posts_path(@user.id)
     end
 
     it 'GET requests response status was correct' do
@@ -11,29 +16,22 @@ RSpec.describe 'Posts', type: :request do
     end
 
     it 'renders a correct template' do
-      expect(response).to render_template(:index)
+      expect(response).to render_template('posts/show')
     end
 
-    it 'response body includes correct placeholder text' do
-      expect(response.body).to include('List of all posts')
+    before(:each) do
+      get user_post_path(@user.id, @post.id)
+    end
+    it 'GET requests response status was correct' do
+      expect(response).to have_http_status(:ok)
     end
 
-    describe 'GET #show' do
-      before(:each) do
-        get '/users/1/posts/1'
-      end
+    it 'renders a correct template' do
+      expect(response).to render_template('posts/show')
+    end
 
-      it 'GET requests response status was correct' do
-        expect(response).to have_http_status(:ok)
-      end
-
-      it 'renders a correct template' do
-        expect(response).to render_template(:show)
-      end
-
-      it 'renders content correctly' do
-        expect(response.body).to include('post Details')
-      end
+    it 'renders content correctly' do
+      expect(response.body).to include('post Details')
     end
   end
 end
